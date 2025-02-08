d370de270c04e3a4674868d40bbe08691c7ace91
Timeless Residents <business@satotakuya.jp>
2025-02-08 22:30:32 +0900

    init commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/handson-analog-visual-timer/',
   plugins: [react(),tailwindcss()],
 })
